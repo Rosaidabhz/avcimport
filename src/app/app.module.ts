fd3066a5fe58b3a3188e7759a89f658713142be8
Rosaida <rosaidabhz@gmail.com>
2022-12-15 11:52:40 -0500

    3 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortadaComponent } from './components/portada/portada.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortadaComponent
+    PortadaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
