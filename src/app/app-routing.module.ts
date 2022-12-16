106b8cf410970c4dd8ea5f1ef80f2e644dc215ea
Rosaida <rosaidabhz@gmail.com>
2022-12-16 16:18:38 -0500

    5 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { PortadaComponent } from './components/portada/portada.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:PortadaComponent
+  },
+  {
+    path:'home',
+    component:PortadaComponent
+  },
+  {
+    path:'sobre',
+    component:SobreComponent
+  },
+  {
+    path:'productos',
+    component:ProductosComponent
+  },
+  {
+    path:'servicios',
+    component:ServiciosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortadaComponent } from './components/portada/portada.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortadaComponent,
-    HeaderComponent
+    HeaderComponent,
+    SobreComponent,
+    ProductosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
